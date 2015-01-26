6af237df58fd3cc105e8c2c158b345238f042a5f
Djodjes <damien.georges@gmail.com>
2015-01-26 11:26:22 +0100

    update bundle

--- DataTableDemo/App_Start/BundleConfig.cs
+++ DataTableDemo/App_Start/BundleConfig.cs
@@ -5,7 +5,6 @@ namespace DataTableDemo
 {
     public class BundleConfig
     {
-        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
@@ -14,8 +13,6 @@ namespace DataTableDemo
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
@@ -29,8 +26,6 @@ namespace DataTableDemo
                       "~/Content/site.css",
                       "~/Content/Datatables/jquery.dataTables.min.css"));
 
-            // Set EnableOptimizations to false for debugging. For more information,
-            // visit http://go.microsoft.com/fwlink/?LinkId=301862
             BundleTable.EnableOptimizations = true;
         }
     }
